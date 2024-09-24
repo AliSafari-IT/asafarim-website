9328e0476caf557d26d2cdcfdece1e115f5aad50
asafarim <asafarim.it@gmail.com>
2024-09-24 18:53:36 +0200

    up

--- techdocs/docusaurus.config.ts
+++ techdocs/docusaurus.config.ts
@@ -38,7 +38,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://asafarim.be",
+  url: "https://asafarim.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: "/",
