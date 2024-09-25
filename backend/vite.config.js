b46aa0f740c01ab99c62d57facb1b331f46ccf48
asafarim <asafarim.it@gmail.com>
2024-09-25 20:00:03 +0200

    add resume-app

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://auth.asafarim.com/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://asafarim.com/' : '/',
   plugins: [
     laravel({
       input: ['resources/js/app.tsx', 'resources/css/app.css', 'resources/css/classes.scss', 'resources/css/md.scss'],
